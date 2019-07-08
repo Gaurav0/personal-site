0527e69b33988f911e9aec0a3ec5499df689b1a0
Gaurav Munjal <Gaurav0@aol.com>
2019-07-08 09:50:45 -0400

    cached shoe

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,8 +1,13 @@
 // app/adapters/application.js
 import DS from 'ember-data';
 import AdapterFetch from 'ember-fetch/mixins/adapter-fetch';
+import CachedShoe from 'ember-cached-shoe';
 
-export default DS.JSONAPIAdapter.extend(AdapterFetch, {
-  host: 'https://gaurav0.github.io',
-  namespace: 'api'
+export default DS.JSONAPIAdapter.extend(AdapterFetch, CachedShoe, {
+  host: 'https://localhost:4200',
+  namespace: 'api',
+  shouldBackgroundReloadRecord() { return false; },
+  shouldBackgroundReloadAll() { return false; },
+  shouldReloadRecord() { return false; },
+  shouldReloadAll() { return false; }
 });
