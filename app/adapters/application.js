65a433d7640f6dcafb468ad3e4f69c9fac563aaf
Gaurav Munjal <Gaurav0@aol.com>
2019-07-08 13:51:00 -0400

    cached shoe service

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -4,12 +4,12 @@ import AdapterFetch from 'ember-fetch/mixins/adapter-fetch';
 import CachedShoe from 'ember-cached-shoe';
 import { inject as service } from '@ember/service';
 
-export default DS.JSONAPIAdapter.extend(CachedShoe, AdapterFetch, {
+export default DS.JSONAPIAdapter.extend(AdapterFetch, CachedShoe, {
   fastboot: service(),
   host: 'http://localhost:4200',
   namespace: 'api',
   shouldBackgroundReloadRecord() { return false; },
   shouldBackgroundReloadAll() { return false; },
-  shouldReloadRecord() { return this.fastboot.isFastBoot; },
-  shouldReloadAll() { return this.fastboot.isFastBoot; }
+  shouldReloadRecord() { return true; },
+  shouldReloadAll() { return true; }
 });
--- app/routes/projects.js
+++ app/routes/projects.js
@@ -1,13 +1,11 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class ProjectsRoute extends Route {
   @service fastboot;
   titleToken = "Projects";
 
   model() {
-    if (this.fastboot.isFastBoot) {
-      return this.store.findAll('project');
-    }
-    return this.store.peekAll('project');
+    return this.modelFor('application');
   }
 }
